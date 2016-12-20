fa1a899c47b1054c113813cba75d560675fc5655
Mathis Hofer <hofer@puzzle.ch>
2016-12-20 15:17:56 +0100

    add angular 2 module and package.json

---
+++ autogrow.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Autogrow } from './autogrow';
+
+@NgModule({
+  declarations: [
+    Autogrow
+  ],
+  exports: [
+    Autogrow
+  ]
+})
+export class AutogrowModule {}
