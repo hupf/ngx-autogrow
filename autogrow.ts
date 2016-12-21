6102c3e067bee9d9e0a0363a5b94d3aac0e8d633
Damian Senn <senn@puzzle.ch>
2016-12-21 14:27:56 +0100

    Add missing semicolons

--- autogrow.ts
+++ autogrow.ts
@@ -90,14 +90,14 @@ export class Autogrow implements AfterViewInit, AfterContentChecked, OnDestroy {
     const dstyle = this.dummy.style,
       ostyle = getComputedStyle(this.el)
 
-    dstyle.whiteSpace = 'pre-wrap'
-    dstyle.boxSizing  = 'border-box'
-    dstyle.wordWrap   = ostyle.getPropertyValue('word-wrap') || 'break-word'
-    dstyle.width      = ostyle.getPropertyValue('width')
-    dstyle.padding    = ostyle.getPropertyValue('padding')
-    dstyle.fontFamily = ostyle.getPropertyValue('font-family')
-    dstyle.fontSize   = ostyle.getPropertyValue('font-size')
-    dstyle.lineHeight = ostyle.getPropertyValue('line-height')
+    dstyle.whiteSpace = 'pre-wrap';
+    dstyle.boxSizing  = 'border-box';
+    dstyle.wordWrap   = ostyle.getPropertyValue('word-wrap') || 'break-word';
+    dstyle.width      = ostyle.getPropertyValue('width');
+    dstyle.padding    = ostyle.getPropertyValue('padding');
+    dstyle.fontFamily = ostyle.getPropertyValue('font-family');
+    dstyle.fontSize   = ostyle.getPropertyValue('font-size');
+    dstyle.lineHeight = ostyle.getPropertyValue('line-height');
   }
 
   prev(el: Node): Node {
