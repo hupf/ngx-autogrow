7290a556ff0507318fc7486f94f45bbaaab9831c
Mathis Hofer <hofer@puzzle.ch>
2019-07-15 10:26:23 +0200

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ngx-autogrow-app');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render textareas', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-autogrow-app!');
+    expect(compiled.querySelectorAll('textarea').length).toBe(3);
   });
 });
